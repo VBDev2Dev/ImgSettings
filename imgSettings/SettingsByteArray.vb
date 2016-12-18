3cb8748c216ea7d7a051dbfc91e4f2f01c9a9906
Jay Asbury <vbjay.net@gmail.com>
2016-12-18 14:26:18 -0500

    Save image in settings

---
+++ imgSettings/SettingsByteArray.vb
@@ -0,0 +1,14 @@
+﻿<Serializable()>
+Public Class SettingsByteArray
+
+    Private _Data As Byte()
+    Public Property Data() As Byte()
+        Get
+            Return _Data
+        End Get
+        Set(ByVal value As Byte())
+            _Data = value
+        End Set
+    End Property
+
+End Class
---
+++ imgSettings/frmImg.vb
@@ -0,0 +1,29 @@
+﻿Public Class frmImg
+    Private Sub frmImg_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If Not doReset Then
+            Using mem As New IO.MemoryStream
+                Dim img As Image = My.Resources.defaultImage
+                img.RotateFlip(RotateFlipType.Rotate180FlipNone)
+                img.Save(mem, Imaging.ImageFormat.Png)
+
+                My.Settings.Img = New SettingsByteArray With {.Data = mem.GetBuffer}
+            End Using
+        End If
+
+    End Sub
+
+    Private Sub frmImg_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        If My.Settings.Img IsNot Nothing Then
+            Using mem As New IO.MemoryStream(My.Settings.Img.Data)
+                pbxCat.Image = CType(Image.FromStream(mem).Clone, Image)
+            End Using
+        End If
+    End Sub
+    Dim doReset As Boolean
+    Private Sub pbxCat_Click(sender As Object, e As EventArgs) Handles pbxCat.Click
+        My.Settings.Reset()
+        My.Settings.Save()
+        doReset = True
+    End Sub
+End Class
